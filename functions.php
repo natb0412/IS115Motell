00cd1f40781673de0180753f89c6a303fd776d39
natb0412 <nabjerrega@uia.no>
2024-11-20 14:56:07 +0100

    Fortsettelse av functions.php
    Navneendringer fra norsk til engelsk. Nye funksjoner

--- functions.php
+++ functions.php
@@ -1,26 +1,38 @@
 <?php
 
-function load_data($fil)
+//Laste inn data
+function load_data($file)
 {
     return include __DIR__ . "/data/" . $file . '.php';
 }
 
+
+//Lagre data
 function save_data($file, $data)
 {
     $content = "<php\nreturn " . var_export($data, true) . ";\n";
     file_put_contents(__DIR__ . "/data/" . $file . ".php", $content);
 }
 
+
+
+//Sjekker om bruker er innlogget
 function is_logged_in()
 {
     return isset($_SESSION["user_id"]);
 }
 
+
+
+//sjekker om bruker er admin
 function is_admin()
 {
     return isset($_SESSION["user_id"]) && $_SESSION["is_admin"];
 }
 
+
+
+//validering for booking. Returner false hvis du ikke oppfyller krav
 function validate_dates($check_in, $check_out)
 {
     $now = newdatetime();
@@ -36,4 +48,13 @@ function validate_dates($check_in, $check_out)
     {
         return false;
     }
+
+    return true;
+}
+
+
+// Logikk for å finde ledige rom
+function find_available_rooms($check_in, $check_out, $adults, $children)
+{
+
 }
