8ecde66c26eee2df2105eb2581d84ea646867ce8
Huskarl10 <martinmal@uia.no>
2024-11-21 13:37:22 +0100

    Oppdaterte index php og css

--- config.php
+++ config.php
@@ -7,9 +7,9 @@ define("Antall_rom", 25);
 
 define("Rom_typer",
 [
-"enkelt" => ["name" => "Enkeltrom", "capacity" => ["adults" => 1, "children" => 1]],
-"dobbelt" => ["name" => "Dobbeltrom", "capacity" => ["adults" => 2, "children" => 1]],
-"Familierom" => ["name" => "Familierom", "capacity" => ["adults" => 2, "children" => 3]] 
+"enkelt" => ["navn" => "Enkeltrom", "kapasitet" => ["voksne" => 1, "barn" => 1]],
+"dobbelt" => ["navn" => "Dobbeltrom", "kapasitet" => ["voksne" => 2, "barn" => 1]],
+"Familierom" => ["navn" => "Familierom", "kapasitet" => ["voksne" => 2, "barn" => 3]] 
 ])
 
 session_start();
