50af4eeab0d953f3efd792e74cd817d6447c22bf
natb0412 <nabjerrega@uia.no>
2024-11-25 21:42:15 +0100

    Endringer schmendringer

--- private/admin/room_admin.php
+++ private/admin/room_admin.php
@@ -5,7 +5,6 @@ require_once "../../public/functions.php";
 
 
 $rooms = load_data("rooms");
-var_dump($rooms);
 
 
 
@@ -49,8 +48,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
                 <input type="text" name="name" value="<?php echo htmlspecialchars($room['name']); ?>" required>
             </label>
             <label>
+                <br>
+                Romtype: <?php echo $room['type']; ?>
+                <br>
+            </label>
+            <label>
                 Beskrivelse:
-                <textarea name="description"><?php echo htmlspecialchars($room['description']); ?></textarea>
+                <textarea name="description"><?php echo htmlspecialchars($room["description"]); ?></textarea>
             </label>
             <button type="submit" name="update_room">Oppdater rom</button>
         </form>
@@ -60,7 +64,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
             <input type="hidden" name="room_id" value="<?php echo $room['id']; ?>">
             <label>
                 Fra dato:
-                <input type="date" name="start_date" required>
+                <input type="date" name="start_date" required value="<?php echo $unavailable_periods["start_date"]; ?>">
             </label>
             <label>
                 Til dato:
---
+++ private/data/booking.php
@@ -0,0 +1,57 @@
+<?php
+return array (
+  0 => 
+  array (
+    'room_id' => '1',
+    'start_date' => '2024-11-26',
+    'end_date' => '2024-11-29',
+  ),
+  1 => 
+  array (
+    'room_id' => '1',
+    'start_date' => '2024-11-26',
+    'end_date' => '2024-11-29',
+  ),
+  2 => 
+  array (
+    'room_id' => '1',
+    'start_date' => '2024-11-26',
+    'end_date' => '2024-11-29',
+  ),
+  3 => 
+  array (
+    'room_id' => '1',
+    'start_date' => '2024-11-26',
+    'end_date' => '2024-11-29',
+  ),
+  4 => 
+  array (
+    'room_id' => '1',
+    'start_date' => '2024-11-26',
+    'end_date' => '2024-11-29',
+  ),
+  5 => 
+  array (
+    'room_id' => '1',
+    'start_date' => '2024-11-26',
+    'end_date' => '2024-11-29',
+  ),
+  6 => 
+  array (
+    'room_id' => '1',
+    'start_date' => '2024-11-26',
+    'end_date' => '2024-11-29',
+  ),
+  7 => 
+  array (
+    'room_id' => '1',
+    'start_date' => '2024-11-26',
+    'end_date' => '2024-11-29',
+  ),
+  8 => 
+  array (
+    'room_id' => '1',
+    'start_date' => '2024-11-26',
+    'end_date' => '2024-11-29',
+  ),
+);
