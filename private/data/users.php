b4030c9aefe47cde1f9a0a328dfa3659b4df65a4
Huskarl10 <martinmal@uia.no>
2024-11-26 22:59:05 +0100

    Addet noen flere ting på index

--- private/data/users.php
+++ private/data/users.php
@@ -1,19 +1,20 @@
 <?php
+// Returns an array of user data
 return array (
     0 => 
     array (
       'user_id' => 1,
-      'username' => 'jinxx22',
+      'username' => 'jx22',
     'name' =>'Albert',
     'password' => 'Test123',
-    ),
+    ),                  // Example user with data
 1 =>
     array (
         'user_id' => 2,
         'username' => '',
       'name' =>'',
       'password' => '',
-      ),
+      ),                // Placeholder for an empty user
 
 );
 ?>
