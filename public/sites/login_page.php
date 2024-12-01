345b572dfa3efd00e519144f512cd3aefb735b91
Huskarl10 <martinmal@uia.no>
2024-12-01 08:28:41 +0100

    Adda test mappe og login_page
    For å teste en test login page, test array og test index.

---
+++ public/sites/login_page.php
@@ -0,0 +1,56 @@
+<?php
+//Includes functions
+require_once "../../public/functions.php";
+
+//loads inn user
+$users = load_data("user");
+
+//Checks if there is an post
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    if(isset($_POST["login"]))
+    {
+      //Retrieve form data for login
+        $login_username = $_POST["login_username"];
+        $login_password = $_POST["login_password"];
+        
+        //Check if username and passwords match
+        if ($username === $login_username && $password === $login_password) 
+        {
+          $_SESSION['user_id'] = $username; //username as session identifier
+          header("dashboard.php");
+          exit();
+        }
+        else 
+        {
+          $error_message = "Invalid username or password";
+          echo $error_message;
+        }
+}
+}
+?>
+
+<!--Link to external CSS file-->
+<link rel="stylesheet" href="sites/css/main.css">
+
+<!--Container for tabs-->
+<div class="tabs">
+  <input type="radio" name="tabs" id="tabone" checked="checked">
+  <label for="tabone">Log in</label>
+  
+  <div class="tab">
+    <h2>Log in</h2>
+    <form method="post" class="vertical_form">
+    
+    <!--Input for username and pasword-->
+    <input type="text" name="login_username" placeholder="Username" required>
+    <input type="password" name="login_password" placeholder="Password" required>
+            
+    <button type="submit" name="login" value="Login"></button>
+    </form>
+       
+    <!--Displays message with link to register page-->
+       <p>Don't have an account? <a href="sites/register_page.php">Register here</a></p>   
+       
+  </div>
+</div>
---
+++ public/sites/test/test_login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <titel> TEST Login Page  </titel>
+</head>
+
+<body>
+<h2>TEST Login Page </h2>
+
+<?php 
+require "test_check.php";
+?>
+
+<form method="post">
+  <input type="text" name="user" require/>
+  <input type="password" name="password" require/>
+  <input type="submit" value="Login"/>
+</form>
+</body>
+</html>
