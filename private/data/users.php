24528e011b2968fba16c8ec29014694779cf2b91
natb0412 <nabjerrega@uia.no>
2024-12-01 21:30:49 +0100

    f

--- private/data/users.php
+++ private/data/users.php
@@ -2,11 +2,20 @@
 return array (
   0 => 
   array (
-    'id' => '674caf8420770',
+    'id' => '1',
     'username' => 'a',
     'name' => 'a',
     'password' => 'a',
     'user_type' => 'guest',
     'is_admin' => false,
+  ),
+  1 => 
+  array (
+    'id' => '674cbdfb1d683',
+    'username' => 'natb',
+    'name' => 'nico',
+    'password' => 'm',
+    'user_type' => 'admin',
+    'is_admin' => true,
   ),
 );
