e50ba373e6d97fd8c2e73db8ba21c1a9b428c10d
natb0412 <nabjerrega@uia.no>
2024-12-02 22:34:19 +0100

    1

--- private/data/users.php
+++ private/data/users.php
@@ -18,4 +18,13 @@ return array (
     'user_type' => 'admin',
     'is_admin' => true,
   ),
+  2 => 
+  array (
+    'id' => '674e2693095b3',
+    'username' => 'd',
+    'name' => 'd',
+    'password' => 'd',
+    'user_type' => 'guest',
+    'is_admin' => false,
+  ),
 );
