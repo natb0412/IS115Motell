23d9ef6023da920c550fa68695edf50d0e6c5d99
natb0412 <nabjerrega@uia.no>
2024-12-02 03:16:51 +0100

    Update booking.php

--- private/data/booking.php
+++ private/data/booking.php
@@ -1,10 +1,33 @@
 <?php
 return array (
-  'id' => '674d06b82f4a4',
-  'room_id' => '1',
-  'guest_name' => 'Admin made unavailable',
-  'start_date' => '2024-12-04',
-  'end_date' => '2024-12-05',
-  'adults' => 0,
-  'children' => 0,
+  0 => 
+  array (
+    'id' => '674d18391225e',
+    'room_id' => '2',
+    'guest_name' => 'Admin made unavailable',
+    'start_date' => '2024-12-02',
+    'end_date' => '2024-12-03',
+    'adults' => 0,
+    'children' => 0,
+  ),
+  1 => 
+  array (
+    'id' => '674d185c58c2a',
+    'room_id' => '3',
+    'guest_name' => 'Admin made unavailable',
+    'start_date' => '2024-12-04',
+    'end_date' => '2024-12-06',
+    'adults' => 0,
+    'children' => 0,
+  ),
+  2 => 
+  array (
+    'id' => '674d18803a9f7',
+    'room_id' => ' 1',
+    'guest_name' => 'x',
+    'start_date' => '2024-12-03',
+    'end_date' => '2024-12-05',
+    'adults' => '1',
+    'children' => '1',
+  ),
 );
